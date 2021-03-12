351391a1f285d424ee9b08002c3745d281628a59
Cl Dg <mobroma@gmail.com>
2021-03-12 08:24:24 +0300

    add

---
+++ hw-2.py
@@ -0,0 +1,6 @@
+def input_data (name, surname, city, email, phone):
+    print(f'{name}, {surname}, {city}, {email}, {phone}')
+
+input_data(email = 'fff@ghgh.com', surname = 'Smith', name = 'Jhon', phone = '123-65-78', city = 'NY')
+
+
